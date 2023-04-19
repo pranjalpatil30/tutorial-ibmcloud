479ae9935b065dea653e8e7d26295388db658f55
Pranjal Patil <pranjalpatil30@gmail.com>
2023-04-19 14:58:28 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,14 +12,14 @@ provider "ibm" {
 }
 
 data "ibm_resource_group" "default_group" {
-  name = "default"
+  name = "Default"
 }
 
 resource "ibm_cloudant" "cloudant" {
   name     = "policy-as-code-cloudant"
   location = "us-south"
   plan     = "lite"
-  # tags     = ["costcenter:001589"]
+  tags     = ["costcenter:001589"]
 
   timeouts {
     create = "15m"
